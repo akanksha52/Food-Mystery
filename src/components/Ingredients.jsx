fc9b34bb75f1c691390386418e97d4eebd88f4c9
akanksha52 <akankshagangrade27@gmail.com>
2025-12-04 15:06:19 +0530

    Update ingredients list as user enters them

---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,8 @@
+export default function Ingredients(props)
+{
+    return (
+        <div>
+            <li>{props.name}</li>
+        </div>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,45 @@
 import { useState } from "react";
+import Ingredients from './Ingredients'
+
 
 export default function Main() {
+  const [ingredientsList, setIngredientsList]=useState(["pepper", "salt", "rice"]);
   const [ingredient, setIngredient]=useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (ingredient.trim()==="") return;
-    setIngredient("");
-  }
+  function HandleSubmit(event)
+  {
+    event.preventDefault();
+    const formData=new FormData(event.currentTarget);
+    const newIngredient=formData.get("ingredient");
+    setIngredientsList(prev => [...prev, newIngredient]);
+  };
+
+  const InElements=ingredientsList.map((ing) => {
+    return <Ingredients key={ing} name={ing}/>
+  });
 
   return (
     <div className="main-container">
-      <form className="ingredient-form" onSubmit={handleSubmit}>
+
+      <form className="ingredient-form" onSubmit={HandleSubmit}>
         <input
           type="text"
           placeholder="eg. Pepper"
           value={ingredient}
           onChange={(e) => setIngredient(e.target.value)}
           className="ingredient-input"
+          aria-label="Add ingredient"
+          name="ingredient"
         />
         <button type="submit" className="add-btn">
           Add
         </button>
       </form>
+
+      <ul>
+        {InElements}
+      </ul>
+
     </div>
   );
 }
